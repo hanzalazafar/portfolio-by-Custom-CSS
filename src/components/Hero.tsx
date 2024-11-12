02897748db189c080aaa784bb9ce76f7c0737eb2
hanzala <hanzalaazafar631@gmail.com>
2024-11-12 14:18:23 +0500

    Initial commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,13 +3,13 @@ import Navbar from './Navbar'
 
 const Hero = () => {
   return (
-    <div id='hero' className='min-h-screen bg-no-repeat bg-[url(/profilepic.jpg)]  bg-cover '
-    style={{ backgroundSize:"40%" ,backgroundPosition:"left 220px top 300px"}}
+    <div id='hero' className='min-h-screen bg-no-repeat bg-[url(/profilepic.jpg)]  bg-cover container right-0  '
+    style={{ backgroundSize:"35%" ,backgroundPosition:"left 20px top 90px"}}
     >
      <Navbar />
-     <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
+     <div className='container left-0 grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
         <div className='hidden lg:block'></div>
-        <div className='text-[80px] sm:text-[110px]  mt-28 ml-56 font-bold leading-tight flex justify-center items-center'>
+        <div className='text-[80px] sm:text-[110px]  mt-28 ml-5 font-bold leading-tight flex justify-center items-center'>
             <div >
                 <p  data-aos='flip-up'className='text-purple-950'> Hello <br />I am</p>
                 <p  data-aos='flip-up'>Hanzala</p>
