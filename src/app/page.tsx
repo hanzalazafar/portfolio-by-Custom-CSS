ff084c84a7c305b3b88023b36ff915cf6b5a42e0
hanzala <hanzalaazafar631@gmail.com>
2024-11-12 13:11:48 +0500

    Update code with fixes for AOS and other changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Hero from "@/components/Hero";
-import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
+'use client';
+import About from '@/components/About';
+import Contact from '@/components/Contact';
+import Hero from '@/components/Hero';
+import Projects from '@/components/Projects';
+import Skills from '@/components/Skills';
 
 import AOS from "aos"
 import "aos/dist/aos.css"
@@ -19,7 +19,7 @@ export default function Home() {
       anchorPlacement:"bottom-bottom",
       offset:160,
     });
-    AOS.refresh();
+    AOS.refresh()
   },[]);
   return (
     <main>
