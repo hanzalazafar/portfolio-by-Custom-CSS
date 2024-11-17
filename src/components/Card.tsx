ad5bc6f9744f3c7f3da601eac8ec97d657f8864a
hanzala <hanzalaazafar631@gmail.com>
2024-11-17 21:14:30 +0500

    Initial commit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,46 +3,49 @@ import Image from 'next/image';
 import '../app/styles/card.css';
 
 interface propsType {
-    title: string;
-    desc: string;
-    img: string;
-    tags: string[];
+  title: string;
+  desc: string;
+  img: string;
+  tags: string[];
 }
 
 const Card: React.FC<propsType> = ({ title, desc, img, tags }) => {
-    const [windowWidth, setWindowWidth] = useState<number>(0);
+  const [windowWidth, setWindowWidth] = useState<number | null>(null);
 
-    // Yeh code client-side pe run hoga jab component mount hoga
-    useEffect(() => {
-        if (typeof window !== 'undefined') { // Ye check karta hai ke code sirf browser mein chale
-            setWindowWidth(window.innerWidth); // Window ka width set karte hain
-        }
-    }, []); // Yeh sirf initial render ke baad chalega
+  // `useEffect` hook to set windowWidth only on the client-side
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setWindowWidth(window.innerWidth); // Set window width when component mounts on client
+    }
+  }, []); // Empty dependency array means it runs only once on mount
 
-    return (
-        <div className={`card ${windowWidth >= 640 ? 'card-sm' : ''}`} data-aos="zoom-in-up">
-            <div>
-                <Image
-                    className={`card-image ${windowWidth >= 640 ? 'card-image-sm' : ''}`}
-                    src={img}
-                    width={350}
-                    height={350}
-                    alt={title}
-                />
+  // If windowWidth is null, render nothing during SSR
+  if (windowWidth === null) return null;
+
+  return (
+    <div className={`card ${windowWidth >= 640 ? 'card-sm' : ''}`} data-aos="zoom-in-up">
+      <div>
+        <Image
+          className={`card-image ${windowWidth >= 640 ? 'card-image-sm' : ''}`}
+          src={img}
+          width={350}
+          height={350}
+          alt={title}
+        />
+      </div>
+      <div className="card-container">
+        <div className="card-title">{title}</div>
+        <div>{desc}</div>
+        <div>
+          {tags.map((el) => (
+            <div className="card-tags" key={el}>
+              {el}
             </div>
-            <div className="card-container">
-                <div className="card-title">{title}</div>
-                <div>{desc}</div>
-                <div>
-                    {tags.map((el) => (
-                        <div className="card-tags" key={el}>
-                            {el}
-                        </div>
-                    ))}
-                </div>
-            </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Card;
